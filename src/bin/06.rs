c6f7fbb273717ea2292f3e244cc61d914df273b2
Chiel Douwes <chieldouwes@gmail.com>
2023-01-28 12:37:01 +0100

    Day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,58 @@
+use std::collections::BTreeSet;
+
+fn is_marker(chars: &[char]) -> bool {
+    let mut set = BTreeSet::new();
+    for char in chars {
+        if !set.insert(char) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn find_window(input: &str, size: usize) -> Option<u32> {
+    input.chars().collect::<Vec<_>>().windows(size).enumerate()
+        .find(|(_, chars)| is_marker(chars))
+        .map(|result| (result.0 + size) as u32)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    find_window(input, 4)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    find_window(input, 14)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(7));
+
+        assert_eq!(part_one("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+        assert_eq!(part_one("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+        assert_eq!(part_one("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
+        assert_eq!(part_one("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), Some(19));
+
+        assert_eq!(part_two("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(23));
+        assert_eq!(part_two("nppdvjthqldpwncqszvftbrmjlhg"), Some(23));
+        assert_eq!(part_two("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(29));
+        assert_eq!(part_two("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(26));
+    }
+}
